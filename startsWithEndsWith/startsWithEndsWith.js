0fbbbe8eb4c7ac7425f16f599a7ca77c2237697e
Antônio Britto <antonio.britto.2000@hotmail.com>
2023-04-10 08:00:32 -0300

    comment

--- startsWithEndsWith/startsWithEndsWith.js
+++ startsWithEndsWith/startsWithEndsWith.js
@@ -1,2 +1,4 @@
 //case sensitve
 //basicamente vai retornar um boolena vendo se uma determinada string começa com uma determinada string ou não
+
+//O node basicamente agora trouxe a possibilidade de poder executar códigos JavaScript fora do navegador
